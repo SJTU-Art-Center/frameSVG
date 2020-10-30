4353d9193c4dc4a3f1d6b2579d33e4df36cc875f
LogCreative <longlaoda123456@qq.com>
2020-10-30 20:37:14 +0800

    skeleton

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app",
+    data:{
+        addr: "",
+        name: "",
+        suffix: "",
+        width: "",
+        height: "",
+        start: "",
+        end: "",
+        fps: "",
+        code: "",
+    },
+});
